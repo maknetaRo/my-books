bd47e298606d320fd3bd4d15aad94e6cbe9c87a2
maknetaRo <makneta@gmail.com>
2018-10-27 13:05:40 +0200

    Created models: genre, book, author

---
+++ books/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.urls import reverse
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text='Enter a book genre')
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for a book')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
