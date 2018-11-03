4c5d18a0b80ceec17848b57ae086da97904203a4
maknetaRo <makneta@gmail.com>
2018-11-03 19:52:28 +0100

    Added navbar and changed login and logout views

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
         # post views
         #path('login/', views.user_login, name='login'),
         path('', views.dashboard, name='dashboard'),
-        path('login/', auth_views.LoginView.as_view(), name='login'),
-        path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+        path('login/', auth_views.LoginView.as_view(template_name='account/registration/login.html'), name='login'),
+        path('logout/', auth_views.LogoutView.as_view(template_name='account/registration/logout.html'), name='logout'),
 
 ]
